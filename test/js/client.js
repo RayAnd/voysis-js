bfabda79ea6d4be0ba4251f890ce24937586c58f
Marco Marignoli <mmarignoli@voysis.com>
2019-12-06 13:10:39 +0000

    added streamingAudioDeadline url params

--- test/js/client.js
+++ test/js/client.js
@@ -30,6 +30,7 @@
         const REFRESH_TOKEN = 'RefreshToken';
         const EMAIL = 'Email';
         const USER_ID = 'UserId';
+        const STREAMING_AUDIO_DEADLINE = 'StreamingAudioDeadline';
         const localStorage = window.localStorage;
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         var audioContext_ = null;
@@ -44,6 +45,7 @@
         var previousQueryContext_;
         var queryConversationId_;
         var sessionChanged_ = false;
+        var streamingAudioDeadline_ = 20000;
         var audioBufferSize_ = 4096;
         var ignoreVad = false;
         var chatbotMode = false;
@@ -177,6 +179,16 @@
                 audioBufferSize_ = audioBufferSize;
             },
 
+            setStreamingAudioDeadline: function (streamingAudioDeadline) {
+                streamingAudioDeadline_ = streamingAudioDeadline;
+                localStorage.setItem(STREAMING_AUDIO_DEADLINE, streamingAudioDeadline);
+                sessionChanged_ = true;
+            },
+            
+            getStreamingAudioDeadline: function (streamingAudioDeadline) {
+                return streamingAudioDeadline_;
+            },
+
             getVoysisSession: function () {
                 if (!voysisSession_ || sessionChanged_) {
                     previousQueryContext_ = null;
@@ -185,7 +197,7 @@
                         host: host_,
                         audioProfileId: audioProfileId_,
                         debugEnabled: true,
-                        streamingAudioDeadline: 10000,
+                        streamingAudioDeadline: streamingAudioDeadline_,
                         autoSendDurations: true,
                         audioBufferSize: audioBufferSize_,
                         userId: userId_,
